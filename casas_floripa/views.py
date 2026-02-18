cc124c2aea1a12bc4daf1c3f4606059fb9ad8792
Rodrigo Vieira <rodrigo.vieira@gmail.com>
2026-02-18 11:39:50 -0300

    Enhance vendas view with optimized query and prefetching
    - Updated the vendas view to include the ItemPedido model for better data retrieval.
    - Implemented selective field loading using .only() for Pedido and prefetching of related ItemPedido objects to improve performance.

--- casas_floripa/views.py
+++ casas_floripa/views.py
@@ -1,7 +1,8 @@
+from django.db.models import Prefetch
 from django.shortcuts import render
 
 from casas_floripa.decorators import measure_time_and_memory
-from casas_floripa.models import Pedido
+from casas_floripa.models import ItemPedido, Pedido
 
 DEFAULT_LIMIT = 10
 
@@ -10,8 +11,26 @@ DEFAULT_LIMIT = 10
 def vendas(request):
     pedidos = (
         Pedido.objects.order_by("-data_criacao")
+        .only(
+            "id",
+            "status",
+            "desconto_pct",
+            "data_entrega",
+            "data_criacao",
+            "cliente_id",
+            "cliente__id",
+            "cliente__nome",
+            "cliente__sobrenome",
+        )
         .select_related("cliente")
-        .prefetch_related("itens")
+        .prefetch_related(
+            Prefetch(
+                "itens",
+                queryset=ItemPedido.objects.only(
+                    "pedido_id", "preco_venda", "quantidade"
+                ),
+            )
+        )
     )
 
     if "all" not in request.GET:
